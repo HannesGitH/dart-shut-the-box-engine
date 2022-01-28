1a2e18f773fd88731aa34ce3ed301e1c7f2a81bb
Hannes vonSarahsLaptop <HannesGitH@users.noreply.github.com>
2022-01-28 20:17:29 +0100

    -

--- lib/shut_the_box_engine.dart
+++ lib/shut_the_box_engine.dart
@@ -1,1 +1,11 @@
 library shut_the_box_engine;
+
+import 'package:riverpod/riverpod.dart';
+
+part 'models/card.dart';
+part 'models/cardSets.dart';
+part 'models/dice.dart';
+part 'models/game.dart';
+part 'models/player.dart';
+
+part 'viewmodels/game.dart';
