de2a95341d701bbed0188165faa2bd72da7720c0
Hannes vonSarahsLaptop <HannesGitH@users.noreply.github.com>
2022-01-28 22:23:28 +0100

    cardsets aint no model

--- lib/shut_the_box_engine.dart
+++ lib/shut_the_box_engine.dart
@@ -6,7 +6,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 part 'models/card.dart';
-part 'models/cardSets.dart';
+part 'cardSets.dart';
 part 'models/dice.dart';
 part 'models/game.dart';
 part 'models/player.dart';
