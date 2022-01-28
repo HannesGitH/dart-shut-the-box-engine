9d5a66e222701a9e55eb5be600d863e061cdd8b1
Hannes vonSarahsLaptop <HannesGitH@users.noreply.github.com>
2022-01-28 22:45:41 +0100

    ...

--- lib/models/card.dart
+++ lib/models/card.dart
@@ -1,8 +1,15 @@
 part of shut_the_box_engine;
 
-class Card {
+class Card extends ChangeNotifier {
   bool isDown;
   int number;
 
   Card({required this.number, this.isDown = false});
+
+  bool select() {
+    bool wasDown = isDown;
+    isDown = true;
+    notifyListeners();
+    return !wasDown;
+  }
 }
--- lib/models/player.dart
+++ lib/models/player.dart
@@ -1,11 +1,29 @@
 part of shut_the_box_engine;
 
-class Player {
+class Player extends ChangeNotifier {
   bool hasWon;
   late List<Card> cards;
+
+  void select(int cardIndex) {
+    if ((cards.at(cardIndex)?.select() ?? false) &&
+        (cards.every((element) => element.isDown))) {
+      hasWon = true;
+      notifyListeners();
+    }
+  }
 
   Player({this.hasWon = false, List<Card>? cardset})
       : cards = cardset ?? defaultCardSet;
 
   bool get finished => cards.every((element) => element.isDown);
+}
+
+extension At<T> on List<T> {
+  T? at(int? index) {
+    try {
+      return this[index!];
+    } catch (e) {
+      return null;
+    }
+  }
 }
